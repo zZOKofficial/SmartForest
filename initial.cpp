fc21ccc66b717570a6a5602ba8c2111da47a8ef6
student <toriq809@gmail.com>
2026-06-14 09:23:50 +0600

    initial setup from bash for university project

---
+++ initial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    cout << " Hello Roarshark"<<endl;
+
+    string name;
+    int age;
+    cout <<"Enter your Name :" ;
+    cin >> name;
+    cout <<"Enter your age :" ;
+    cin>>age;
+    cout <<" My Name is "<< name<<endl;
+    cout << "My Age is " << age<<endl;
+
+    getch();
+
+    //return 0;
+
+
+}
